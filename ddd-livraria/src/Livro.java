6395a280fcc781fb1e7b1e845165442971f4e6f1
Felipe Maglio <felipemagliofilho@gmail.com>
2025-03-13 12:22:34 -0300

    Add files via upload

---
+++ ddd-livraria/src/Livro.java
@@ -0,0 +1,11 @@
+public class Livro {
+    String titulo;
+    String autor;
+    double preco;
+    String editora;
+    int paginas;
+    String resumo;
+    String tipoCapa;
+
+
+}
---
+++ ddd-livraria/src/TesteLivro.java
@@ -0,0 +1,18 @@
+public class TesteLivro {
+    public static void main(String[] args) {
+        Livro meuLivro = new Livr ();
+        meuLivro.titulo = "UML 2";
+        meuLivro.autor = "Gilleanes Guedes";
+        meuLivro.editora = "novatec";
+        meuLivro.preco = 50;
+        meuLivro.paginas = 484;
+        meuLivro.tipoCapa = "Capa dura";
+        meuLivro.resumo = "UML abordagem prática...";
+
+        Livro livroDaGiovanna = new Livro();
+        livroDaGiovanna.titulo = "Harry Potter e a pedra filosofal";
+        livroDaGiovanna.autor = "JK";
+
+
+    }
+}
